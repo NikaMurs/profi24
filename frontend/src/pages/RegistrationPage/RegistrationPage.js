d94df73c4907ba155cb17ca80faab70af88a4d66
Akim Vakhrenev <akeem3159119@gmail.com>
2024-02-21 18:57:18 +0300

    required to required form

--- frontend/src/pages/RegistrationPage/RegistrationPage.js
+++ frontend/src/pages/RegistrationPage/RegistrationPage.js
@@ -9,18 +9,18 @@ export default function RegistrationPage() {
                 <div className="registrateContent">
                     <form>
                         <input className="registrateFormInput" type="" name="f_name" id="f_name" placeholder="Фамилия*" required />
-                        <input className="registrateFormInput" type="" name="name" id="name" placeholder="Имя*" />
-                        <input className="registrateFormInput" type="" name="t_name" id="t_name" placeholder="Отчество*" />
-                        <input className="registrateFormInput" type="" name="country" id="country" placeholder="Страна*" />
-                        <input className="registrateFormInput" type="" name="city" id="city" placeholder="Город*" />
-                        <input className="registrateFormInput" type="" name="tel" id="tel" placeholder="Моб. телефон*" />
-                        <input className="registrateFormInput" type="" name="mail" id="mail" placeholder="Эл. почта*" />
+                        <input className="registrateFormInput" type="" name="name" id="name" placeholder="Имя*" required/>
+                        <input className="registrateFormInput" type="" name="t_name" id="t_name" placeholder="Отчество*" required/>
+                        <input className="registrateFormInput" type="" name="country" id="country" placeholder="Страна*" required/>
+                        <input className="registrateFormInput" type="" name="city" id="city" placeholder="Город*" required/>
+                        <input className="registrateFormInput" type="" name="tel" id="tel" placeholder="Моб. телефон*" required/>
+                        <input className="registrateFormInput" type="" name="mail" id="mail" placeholder="Эл. почта*" required/>
                         <input className="registrateFormInput" type="" name="site" id="" placeholder="Сайт" />
                         <input className="registrateFormInput" type="" name="vk" id="" placeholder="Вк" />
                         <input className="registrateFormInput" type="" name="profession" id="" placeholder="Род деятельности" />
                         <input className="registrateFormInput" type="" name="count_book" id="" placeholder="Сколько книг в год?" />
-                        <input className="registrateFormInput" type="password" name="pasw" id="" placeholder="Пароль*" />
-                        <input className="registrateFormInput" type="password" name="c_pasw" id="" placeholder="Подтвердите пароль*" />
+                        <input className="registrateFormInput" type="password" name="pasw" id="" placeholder="Пароль*" required/>
+                        <input className="registrateFormInput" type="password" name="c_pasw" id="" placeholder="Подтвердите пароль*" required/>
                         <p className="registrateFormText">*-поля, обязательные для заполнения</p>
                     </form>
                     <button className="registrateButton" onclick="cod();">
