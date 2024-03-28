504151ff2a76a4c2cf0951af1878ee696b7c2725
Akim Vakhrenev <akeem3159119@gmail.com>
2024-03-28 17:19:29 +0300

    o

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -36,50 +36,67 @@ export default function LoginPage() {
         setPassword(e.currentTarget.value)
     }
 
-function handleClick() {
-    const postData = {
-        username: formatPhoneNumber(login),
-        password: password
-    };
+    function handleClick() {
+        const postData = {
+            username: formatPhoneNumber(login),
+            password: password
+        };
 
-    if (postData.username < 11) { alert('Вы не заполнили поле: Телефон'); return };
-    if (postData.password === '') { alert('Вы не заполнили поле: Пароль'); return };
+        if (postData.username < 11) { alert('Вы не заполнили поле: Телефон'); return };
+        if (postData.password === '') { alert('Вы не заполнили поле: Пароль'); return };
 
-    // if (login === '123' && password === '456') {
-    //     document.cookie = "isUserLoged=true; path=/;"
-    //     navigate(0)
-    // } else {
-    //     alert('Неверные данные')
-    // }
-}
+        fetch(`${process.env.REACT_APP_URL}/lpgin`, {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Ошибка соединения');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Response:', data);
+                //     document.cookie = "isUserLoged=true; path=/;"
+                //     navigate(0)
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+                alert('Произошла ошибка')
+            });
+    }
 
 
-return (
-    <div className="signInCenter">
-        <div className="signIn">
-            <div className="signInHeader">
-                <h1 className="signInHeaderText">Вход</h1>
-            </div>
-            <div className="signInMain">
-                <form>
-                    <input className="signInFormInput phoneIcon" type="text" name="login" id="login" placeholder="введите номер телефона..." value={login} onChange={onChangePhone} />
-                    <input className="signInFormInput passwordIcon" type="password" name="password" id="" placeholder="пароль..." value={password} onChange={onChangePassword} />
-                </form>
-
-                <div className="signInPasswordWindow">
-                    <label className="signInRemeberPasswordGroup">
-                        <input type="checkbox" className="signInRemeberPassword" />
-                        <span className="signInRemeberPasswordText">Запомнить пароль?</span>
-                    </label>
-                    <a className="signInForgotPassword" href="#0">Забыли пароль?</a>
+    return (
+        <div className="signInCenter">
+            <div className="signIn">
+                <div className="signInHeader">
+                    <h1 className="signInHeaderText">Вход</h1>
                 </div>
+                <div className="signInMain">
+                    <form>
+                        <input className="signInFormInput phoneIcon" type="text" name="login" id="login" placeholder="введите номер телефона..." value={login} onChange={onChangePhone} />
+                        <input className="signInFormInput passwordIcon" type="password" name="password" id="" placeholder="пароль..." value={password} onChange={onChangePassword} />
+                    </form>
 
-                <button className="signInButton" onClick={handleClick}>
-                    Войти
-                </button>
-                <a className="signInRegistrate" href="/registration">Зарегестрироваться</a>
+                    <div className="signInPasswordWindow">
+                        <label className="signInRemeberPasswordGroup">
+                            <input type="checkbox" className="signInRemeberPassword" />
+                            <span className="signInRemeberPasswordText">Запомнить пароль?</span>
+                        </label>
+                        <a className="signInForgotPassword" href="#0">Забыли пароль?</a>
+                    </div>
+
+                    <button className="signInButton" onClick={handleClick}>
+                        Войти
+                    </button>
+                    <a className="signInRegistrate" href="/registration">Зарегестрироваться</a>
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
 }
