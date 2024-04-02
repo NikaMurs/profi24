5e514912f8a66252d924b4605cda9e839c385c9a
Akim Vakhrenev <akeem3159119@gmail.com>
2024-04-02 14:56:04 +0300

    patch with userDopInfo added

--- frontend/src/pages/LkEditPage/LkEditPage.js
+++ frontend/src/pages/LkEditPage/LkEditPage.js
@@ -45,30 +45,28 @@ export default function LkEditPage() {
     }
 
     function handleOnSave(e) {
-        console.log(data)
-
-        // fetch(`${process.env.REACT_APP_URL}/lk/edit`, {
-        //     method: 'PATCH',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         Authorization: `Bearer ${getCookie('authorization')}`
-        //     },
-        //     body: JSON.stringify(data)
-        // })
-        //     .then(response => {
-        //         if (!response.ok) {
-        //             throw new Error('Ошибка соединения');
-        //         }
-        //         return response.json();
-        //     })
-        //     .then(data => {
-        //         navigate('/lk')
-        //     })
-        //     .catch(error => {
-        //         console.error('There was an error!', error);
-        //         alert('Произошла ошибка')
-        //     });
-        // e.preventDefault()
+        fetch(`${process.env.REACT_APP_URL}/lk/edit`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${getCookie('authorization')}`
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Ошибка соединения');
+                }
+                return response.json();
+            })
+            .then(data => {
+                navigate('/lk')
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+                alert('Произошла ошибка')
+            });
+        e.preventDefault()
     }
 
     return (
