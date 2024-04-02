07ee467afb99832a2a305d429f18e2837fbe909c
Pashckevich227 <iport234@gmail.com>
2024-04-02 22:22:38 +0300

    add cors

--- backend/main.py
+++ backend/main.py
@@ -14,10 +14,13 @@ app.add_middleware(
     allow_origins=[
         "http://localhost",
         "http://localhost:8080",
+        "http://localhost:3000",
+        "http://localhost:3001",
         "http://127.0.0.1",
         "http://127.0.0.1:8080",
         "http://5.35.84.51",
-        "http://5.35.84.51:8080"
+        "http://5.35.84.51:8080",
+        "http://profi24test.profi24.beget.tech"
     ],
     allow_credentials=True,
     allow_methods=["*"],
